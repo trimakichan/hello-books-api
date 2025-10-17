5adc8a1bb6723d25a901b11e2200250d4c140f7c
Makiko <trimakichan@gmail.com>
2025-10-16 20:03:34 -0700

    create books endpoint

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,11 @@
 from flask import Flask
+from .routes.book_routes import books_bp
+# from .routes.hello_world_routes import hello_world_bp
 
 def create_app():
     app = Flask(__name__)
+
+    app.register_blueprint(books_bp)
+    # app.register_blueprint(hello_world_bp)
 
     return app
