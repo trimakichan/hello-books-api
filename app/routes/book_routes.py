b4aa5462a970552bd85bd60377d9d65b25759132
Makiko <trimakichan@gmail.com>
2025-10-23 21:23:13 -0700

    complete until 03c-read-all-books

--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,10 +1,14 @@
-from flask import Blueprint, abort, make_response
-from ..models.book import books
+from flask import Blueprint, abort, make_response, request
+from app.models.book import Book
+from ..db import db
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-@books_bp.get("")
+@books_bp.get("/")
 def get_all_books():
+    query = db.select(Book).order_by(Book.id)
+    books = db.session.scalars(query)
+
     books_response = []
     for book in books:
         books_response.append(
@@ -16,27 +20,48 @@ def get_all_books():
         )
     return books_response
 
+# @books_bp.get("/<book_id>")
+# def get_one_book(book_id):
+#     book = validate_book(book_id)
 
-@books_bp.get("/<book_id>")
-def get_one_book(book_id):
-    book = validate_book(book_id)
+#     return {
+#         "id": book.id,
+#         "title": book.title,
+#         "description": book.description,
+#     }
 
-    return {
-        "id": book.id,
-        "title": book.title,
-        "description": book.description,
+# def validate_book(book_id):
+#     try:
+#         book_id = int(book_id)
+#     except ValueError:
+#         response = {"msg": f"book {book_id} invalid"}
+#         abort(make_response(response, 400))
+
+#     for book in books:
+#         if book.id == book_id:
+#             return book
+        
+#     response = {"msg": f"book {book_id} not found"}
+#     abort(make_response(response, 404))
+
+@books_bp.post("/")
+def create_book():
+    request_body = request.get_json()
+    try:
+        title = request_body["title"]
+        description = request_body["description"]
+    except KeyError:
+        msg = {"message": "please provide a valid title and description"}
+        abort(make_response(msg, 400))
+
+    new_book = Book(title=title, description=description)
+    db.session.add(new_book)
+    db.session.commit()
+
+    response = {
+        "id": new_book.id,
+        "title": new_book.title,
+        "description": new_book.description
     }
 
-def validate_book(book_id):
-    try:
-        book_id = int(book_id)
-    except ValueError:
-        response = {"msg": f"book {book_id} invalid"}
-        abort(make_response(response, 400))
-
-    for book in books:
-        if book.id == book_id:
-            return book
-        
-    response = {"msg": f"book {book_id} not found"}
-    abort(make_response(response, 404))
+    return response, 201
